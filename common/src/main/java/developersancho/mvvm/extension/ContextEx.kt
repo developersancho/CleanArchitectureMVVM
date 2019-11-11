f5824f5f8099b05c761e48d4565085e2b503553e
developersancho <mesut.genc@mobillium.com>
2019-11-11 16:35:00 +0300

    created common layer

---
+++ common/src/main/java/developersancho/mvvm/extension/ContextEx.kt
@@ -0,0 +1,12 @@
+package developersancho.mvvm.extension
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showShortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showLongToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
---
+++ common/src/main/java/developersancho/mvvm/extension/StringEx.kt
@@ -0,0 +1,2 @@
+package developersancho.mvvm.extension
+
